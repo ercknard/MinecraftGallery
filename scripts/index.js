7b6da96b5b6e0ea2e0ce938509aed22358b57833
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2022-10-30 16:12:21 +0800

    12

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+/*
+
+$('.tumb > img').each(function() {
+  $(this).css({
+    opacity: 0
+  }).load(function() {
+    $(this).animate({
+      opacity: 1
+    }, 1000);
+  }).attr('src', $(this).data('src'))
+  // wait and remove data-src
+  .delay(100)
+  .attr('data-src','');
+});
+
+*/
---
+++ scripts/loader.js
@@ -0,0 +1,9 @@
+const fadeOut = () => {
+    const loaderWrapper = document.querySelector('.wrapper');
+    loaderWrapper.classList.add('fades');
+  }
+  
+  window.addEventListener('load',fadeOut);
+
+
+  
