6d5f0a88bf3a5fde9dc02838e10aabad4957e4b1
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2022-11-06 22:45:44 +0800

    12

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,11 @@ function showSlide(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
     slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
 
 function showSlides() {
