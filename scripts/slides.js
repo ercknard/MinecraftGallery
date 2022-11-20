4e3c2604835f7db74e0f891fc62b4bf879b60afb
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2022-11-20 19:03:41 +0800

    12

--- scripts/slides.js
+++ scripts/slides.js
@@ -10,5 +10,32 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1} 
   x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel, 7500); 
+  setTimeout(carousel, 15000); 
+}
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlide(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+  autoScroll = false;
+  var i;
+  var slides = document.getElementsByClassName("mySlides-1");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
