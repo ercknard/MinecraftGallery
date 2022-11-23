d518f1df8a62af42dc25f71c14ac3d0977cfd520
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2022-11-23 11:56:00 +0800

    12

--- scripts/particles-1.js
+++ scripts/particles-1.js
@@ -2,7 +2,7 @@ particlesJS(
     "particles-js-1", {
         "particles":{
             "number":{
-                "value":100,
+                "value":50,
                 "density":{
                     "enable":true,
                     "value_area":800}},
@@ -15,4 +15,4 @@ particlesJS(
                             "stroke":{
                                 "width":0,
                                 "color":"#000000"},
-                                "polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":50,"opacity_min":0,"sync":false}},"size":{"value":1,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":.1,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600}}}});
+                                "polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":10,"opacity_min":0,"sync":false}},"size":{"value":1.5,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":.1,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600}}}});
