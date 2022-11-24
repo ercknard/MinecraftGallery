f38d2c0ee57baec796087e49fc4fe455946c7772
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2022-11-24 11:20:18 +0800

    12

--- scripts/typing-contact.js
+++ scripts/typing-contact.js
@@ -38,4 +38,4 @@ function erase() {
 
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
-});
+}); 
